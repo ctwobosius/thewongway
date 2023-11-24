ce39594e0b16136b64be572b72dd8ce1efdcf5f4
Calvin Wong <ctwong958@berkeley.edu>
2023-11-23 18:29:11 -0800

    Attempt github pages deployment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import { AppShell, AppBar, TabGroup, TabAnchor } from '@skeletonlabs/skeleton';
 	import Home from "$lib/asset/home.svg"
 	import Code from "$lib/asset/code.svg"
+	import { base } from '$app/paths';
 </script>
 
 <style>
@@ -35,11 +36,11 @@
 				border=""
 				class="bg-surface-100-800-token w-full"
 				>
-					<TabAnchor href="/" target="_self">
+					<TabAnchor href="{base}/" target="_self">
 						<span class="centered"><img src={Home} alt=""/></span>
 						<span>Home</span>
 					</TabAnchor>
-					<TabAnchor href="/cpp" target="_self">
+					<TabAnchor href="{base}/cpp" target="_self">
 						<span class="centered"><img src={Code} alt=""/></span>
 						<span>C++</span>
 					</TabAnchor>
