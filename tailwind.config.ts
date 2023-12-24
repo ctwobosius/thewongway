51d621a171922229a54d4ab436cf25a3aae907fa
Calvin Wong <ctwong958@berkeley.edu>
2023-12-23 18:06:47 -0800

    Make <code> unaffected by tailwind typography style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,19 @@ export default {
 	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
 	theme: {
-		extend: {},
+		extend: {
+			typography: {
+				DEFAULT: {
+					css: {
+						pre: false,
+						code: false,
+						'pre code': false,
+						'code::before': false,
+						'code::after': false
+					}
+				}
+			}
+		},
 	},
 	plugins: [
 		typography,
