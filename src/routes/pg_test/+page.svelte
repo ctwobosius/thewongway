ee848879aa5117a0cc3754c4aacbc95b73082394
Calvin Wong <ctwong958@berkeley.edu>
2024-01-15 21:37:29 -0800

    Walking guy (dirty code)

--- src/routes/pg_test/+page.svelte
+++ src/routes/pg_test/+page.svelte
@@ -1,4 +1,64 @@
-<script>
+<script lang="ts">
+	import Disapprove from '$lib/asset/disapprove_pixabay.svg';
+	import ThumbsUpHarold from '$lib/asset/thumbs_up_harold.jpg'
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	import Stars from '$lib/components/stars.svelte';
+
+	import hljs from 'highlight.js/lib/core';
+	import 'highlight.js/styles/monokai.css';
+
+	// Import each language module you require
+	import python from 'highlight.js/lib/languages/python';
+
+  import CodeTutorial from '../code_tutorial.svelte'
+  import Spoiler from '../text_spoiler.svelte'
+  import ImageSpoiler from '../img_spoiler.svelte'
+
+	storeHighlightJs.set(hljs);
+	hljs.registerLanguage('python', python);
+
+	const sections_ = {
+		"Programming? Some kind of brainwashing?" : [
+			"Programming boils down to you giving a collection of special rocks instructions on doing something cool (or maybe stupid, or stupidly cool).",
+			"Obviously, I'm oversimplifying, but first, let's let you yeet some words onto the screen."
+		],
+		"Yeeting Words onto a Screen": [
+			"We're gonna use a language called Python."
+		],
+		'Title 2': {
+			'Subtitle 4': ['test']
+		},
+	};
+
+  import { onMount } from 'svelte';
+
+  let targetElements: HTMLElement[] = [];
+  let inViewStates: boolean[] = [];
+
+  const handleIntersection = (index: number, entry: IntersectionObserverEntry) => {
+    inViewStates[index] = entry.isIntersecting;
+
+    if (entry.isIntersecting) {
+      targetElements[index].classList.add('fade-in');
+    } else {
+      targetElements[index].classList.remove('fade-in');
+    }
+  };
+
+  onMount(() => {
+    const observers = targetElements.map((element, index) => {
+      const observer = new IntersectionObserver((entries) => handleIntersection(index, entries[0]));
+      observer.observe(element);
+      return observer;
+    });
+
+    // Cleanup the observers when the component is destroyed
+    return () => {
+      observers.forEach(observer => observer.disconnect());
+    };
+  });
+
+
   let rows = [
     { items: ['Item 1', 'Item 2', 'Item 3'] },
     { items: ['Item 4', 'Item 5'] },
